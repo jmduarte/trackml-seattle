c06b17fa74bcd8ab9a65ec27ba32e9e0b1a82497
Javier Duarte <jduarte1@fnal.gov>
2018-03-21 03:26:36 +0000

    add hit, truth generator

--- dataset.py
+++ dataset.py
@@ -45,6 +45,21 @@ def read_hits(filename, separated_pixels=False):
     df_data.columns = header[:9]
     return df_data
 
+def read_truth(filename):
+    if filename.endswith(".gz"):
+        fp = gzip.open(filename, 'rt')
+    elif filename.endswith(".bz2"):
+        fp = bz2.open(filename, 'rt')
+    else:
+        fp = open(filename)
+
+    if fp is not None:
+        header = [x.strip() for x in fp.readline().split(',')]
+        df_data = pd.DataFrame( [ast.literal_eval(line) for line in fp if line.strip()] )
+        df_data.columns = header
+        fp.close()
+    return df_data
+
 
 def load_hit_generator(data_path, **kw):
     """
@@ -59,6 +74,22 @@ def load_hit_generator(data_path, **kw):
     for name, file in zip(name_list, file_list):
         event_id = int(name[5:])
         yield event_id, read_hits(file, **kw)
+        
+        
+def load_hit_truth_generator(data_path, **kw):
+    """
+    Provide an iterator over all events in a dataset directory.
+
+    For each event it returns the event_id and the output of `load_event`
+    concatenated as a single tuple.
+    """
+    file_list = glob.glob(os.path.join(data_path, 'event*-hits*'))
+    truth_file_list = glob.glob(os.path.join(data_path, 'event*-truth*'))
+    name_list = set(os.path.basename(file).split('-', 1)[0] for file in file_list)
+    name_list = sorted(name_list)
+    for name, file, truth_file in zip(name_list, file_list, truth_file_list):
+        event_id = int(name[5:])
+        yield event_id, read_hits(file, **kw), read_truth(truth_file)
 
 
 def load_hits(data_path):
